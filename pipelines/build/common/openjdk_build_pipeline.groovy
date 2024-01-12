806552c8142a1a47ff5d45b2d860f8c4f5451c0a
Adam Brousseau <adam.brousseau88@gmail.com>
2024-01-12 15:59:09 -0500

    add context to usernamePassword bit
    and a bit of debug cleanup
    
    Signed-off-by: Adam Brousseau <adam.brousseau88@gmail.com>

--- pipelines/build/common/openjdk_build_pipeline.groovy
+++ pipelines/build/common/openjdk_build_pipeline.groovy
@@ -1966,20 +1966,10 @@ class Build {
                                     if (DEFAULTS_JSON['bundle-openjceplus'] == true) {
                                         buildArgs = ' --bundle-openjceplus'
                                     }
-                                    context.println("Here")
-                                    try{
-                                        context.withCredentials([usernamePassword(credentialsId: '7c1c2c28-650f-49e0-afd1-ca6b60479546', passwordVariable: 'GSKIT_PASSWORD', usernameVariable: 'GSKIT_USERNAME')]) {
-                                            context.println("here also")
-                                        }
-                                    } catch(e) {
-                                        context.println(e)
-                                        throw e
-                                    }
-                                    context.println("and here")
                                     context.withEnv(['BUILD_ARGS=' + buildArgs]) {
                                         context.sshagent(['83181e25-eea4-4f55-8b3e-e79615733226']) {
                                             try{
-                                                context.withCredentials([usernamePassword(credentialsId: '7c1c2c28-650f-49e0-afd1-ca6b60479546', passwordVariable: 'GSKIT_PASSWORD', usernameVariable: 'GSKIT_USERNAME')]) {
+                                                context.withCredentials([context.usernamePassword(credentialsId: '7c1c2c28-650f-49e0-afd1-ca6b60479546', passwordVariable: 'GSKIT_PASSWORD', usernameVariable: 'GSKIT_USERNAME')]) {
                                                     context.sh(script: "./${DEFAULTS_JSON['scriptDirectories']['buildfarm']}")
                                                 }
                                             } catch(e) {
