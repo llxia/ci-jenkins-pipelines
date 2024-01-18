a613f6ef73caff1a9f443359ffb33d1a70659f67
Adam Brousseau <adam.brousseau88@gmail.com>
2024-01-18 15:45:38 -0500

    Temp comment out withenv from jceplus change
    debug why build args is missing values for release
    
    Signed-off-by: Adam Brousseau <adam.brousseau88@gmail.com>

--- pipelines/build/common/openjdk_build_pipeline.groovy
+++ pipelines/build/common/openjdk_build_pipeline.groovy
@@ -1971,13 +1971,13 @@ class Build {
                                             openjceplusBuildArgs = '--bundle-openjceplus'
                                         }
                                     }
-                                    context.withEnv(['BUILD_ARGS=' + openjceplusBuildArgs]) {
+                                    //context.withEnv(['BUILD_ARGS=' + openjceplusBuildArgs]) {
                                         context.sshagent(['83181e25-eea4-4f55-8b3e-e79615733226']) {
                                             context.withCredentials([context.usernamePassword(credentialsId: '7c1c2c28-650f-49e0-afd1-ca6b60479546', passwordVariable: 'GSKIT_PASSWORD', usernameVariable: 'GSKIT_USERNAME')]) {
                                                 context.sh(script: "./${DEFAULTS_JSON['scriptDirectories']['buildfarm']}")
                                             }
                                         }
-                                    }
+                                    //}
                                 } else {
                                     context.sh(script: "./${DEFAULTS_JSON['scriptDirectories']['buildfarm']}")
                                 }
